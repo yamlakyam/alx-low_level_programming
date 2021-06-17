5c516800f8af6ae4722410881693c5bbd0920a5c
yamlakyam <yamlakyam@gmail.com>
2021-06-17 01:14:17 -0700

    betty coding style -no trailing white space in comments, no space @ start of line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * outputing a string using puts 
+ * outputing a string using puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 
-    puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-    return (0);
+return (0);
 }
