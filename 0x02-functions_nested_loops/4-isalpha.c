9a81cd2ecfe70ee7f8396c151034cbdfab464048
yamlakyam <yamlakyam@gmail.com>
2021-06-22 05:40:36 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <ctype.h>
+/**
+ * _isalpha - checks whether a character is an alphabet or not
+ *
+ * @c: c is an ascii character
+ *
+ * Return: 1 if c is a letter
+ */
+
+int _isaplha(int c)
+{
+
+if(isalpha(c))
+return (1);
+else
+return (0);
+}
