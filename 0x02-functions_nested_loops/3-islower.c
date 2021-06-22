e8b65b4c6bb3ff76bae27f9bdeaaf1ee63f714ae
yamlakyam <yamlakyam@gmail.com>
2021-06-22 05:21:59 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <ctype.h>
+/**
+ * int _islower - checks whether a character is upper case or lowercase
+ * 
+ * Return: 1 if c is lower case
+ */
+int _islower(int c){
+if(islower(c))
+return 1;
+else
+return 0;
+}
