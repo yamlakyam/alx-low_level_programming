8e4ec832ffb4dde62dedfa935bef12e144daba4b
yamlakyam <yamlakyam@gmail.com>
2021-06-22 04:40:16 -0700

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
+ * 
  *
- *
- *
+ * Description: prints the alphabet
  */
 
 void print_alphabet(void)
 {
 char c;
-for(c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
+_putchar('\n');
