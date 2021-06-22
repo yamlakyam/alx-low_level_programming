69a28d3fcf8f0e0315fc9877a325ff506ba673d0
Yamlak Asrat <60112251+yamlakyam@users.noreply.github.com>
2021-06-22 13:55:58 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,23 +1,17 @@
 #include "holberton.h"
 /**
-
  * headers go here
  * main-entry point function
  * return: 0-success
  */
-
 int main(void)
 {
-
 char holberton[9] = "Holberton";
-int i;
-  
+int i; 
 for(i=0;i<9;i++)
 {
 _putchar(holberton[i]);
-
 }
 _putchar('\n');
-
 return (0);
 }
