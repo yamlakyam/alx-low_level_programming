f151335f80c788e8508d6ca708fe4641fb664e2e
yamlakyam <yamlakyam@gmail.com>
2021-06-22 06:16:48 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <ctype.h>
+
+/**
+ * _abs - returns the absolute value of an int
+ * @n: an int whos abs value is to be computed
+ *
+ * Return: positive integer
+ */
+
+int _abs(int n)
+{
+return(abs(n));
+}
