529a820e3e2d8cb2a4d6601989a0e853beb63e0a
yamlakyam <yamlakyam@gmail.com>
2021-06-22 04:31:20 -0700

    a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; c++)
+_putchar(c);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,6 +2,6 @@
 #define HOLBERTON_H
 
 char _putchar(char c);
-
+void print_alphabet(void);
 
 #endif
