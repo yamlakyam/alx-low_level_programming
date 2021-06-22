f0cd2dad1cf465a4d64177fbef86401909aa5706
Yamlak Asrat <60112251+yamlakyam@users.noreply.github.com>
2021-06-22 17:15:11 +0300

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -5,11 +5,13 @@ char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int add(int, int);
 void print_to_98(int n);
+void times_table(void);
+void jack_bauer(void);
 
 #endif
