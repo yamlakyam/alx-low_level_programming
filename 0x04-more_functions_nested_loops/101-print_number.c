fbe54f9a12cd1e06e7471673931a4f776ce50edb
Yamlak Asrat <60112251+yamlakyam@users.noreply.github.com>
2021-06-25 22:15:40 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,21 @@
 #include "holberton.h"
 /**
- * print_number - a function that prints an integer.
+ *print_number - print integer.
+ *@n: integer.
  *
- * @n: variabole of the function.
- *
- * Return: void
+ *Return: void.
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
+unsigned int x;
 
-	if (n >= 10000)
-	{
-		_putchar(((n / 10000)  % 10) + '0');
-	}
-	if (n >= 1000)
-	{
-		_putchar(((n / 1000)  % 10) + '0');
-	}
-	if (n >= 100)
-	{
-		_putchar(((n / 100)  % 10) + '0');
-	}
-	if (n >= 10)
-	{
-		_putchar(((n / 10)  % 10) + '0');
-	}
-	_putchar(n % 10 + '0');
+x = n;
+if (n < 0)
+{
+_putchar(45);
+x = -n;
+}
+if (x / 10)
+print_number(x / 10);
+_putchar((x % 10) + '0');
 }
